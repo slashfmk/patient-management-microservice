89156559a94ff1d3afd8d7f6da99cb2b6596218d
slashfmk <slashcs7@hotmail.com>
2026-08-21 04:28:21 -0400

    Api http endpoint created for patients

---
+++ patient-service/src/main/java/com/oxygenik/patientservice/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.oxygenik.patientservice.controller;
+
+
+import com.oxygenik.patientservice.dto.PatientResponseDTO;
+import com.oxygenik.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        return ResponseEntity.ok(patientService.getPatients());
+    }
+}
