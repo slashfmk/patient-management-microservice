589546c386f5157eb5427adb987a9a2155fc662d
slashfmk <slashcs7@hotmail.com>
2026-09-13 20:38:32 -0400

    Billing service - create-billing-account implemented

---
+++ billing-service/src/main/java/com/oxygenik/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package com.oxygenik.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.grpc.server.service.GrpcService;
+
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger log =  LoggerFactory.getLogger(BillingGrpcService.class.getName());
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+        // Business logic - e.g save to database, perform calculates etc...
+        var response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
